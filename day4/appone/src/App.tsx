25baef03385575f3329b2c10520354c8b760bbdb
Dhruv Kumar <dhruvlol12@gmail.com>
2022-05-24 10:19:06 +0530

    2nd update

--- day4/appone/src/App.tsx
+++ day4/appone/src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import AddUser from './component/AddUser';
 import Messagebox from './component/Messagebox';
+import { Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
+const queryClient = new QueryClient()
 function App() {
   return (
+   
     <div className="App">
-      {/* <AddUser /> */}
-      <Messagebox />
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+            <Route path='/' element={< Messagebox />} ></Route>
+          <Route path='/adduser' element={<AddUser /> } > </Route>
+        </Routes>
+      </QueryClientProvider>
     </div>
   );
 }
